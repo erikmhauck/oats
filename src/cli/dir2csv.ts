42df43c885097b7cf2f0ad9766fcd936da5dbb51
Erik Hauck <erikmhauck@gmail.com>
2022-10-12 20:49:38 -0500

    new features added to dir2csv

--- src/cli/dir2csv.ts
+++ src/cli/dir2csv.ts
@@ -1,10 +1,48 @@
+import Logger from "../api/logger";
 import walkSync from "walk-sync";
-import { writeFileSync } from "fs";
+import { existsSync } from "fs";
+import { writeCSVWithRetryPrompt } from "../api/csvUtils";
 
-export const dir2csv = (target: string, output: string) => {
-  console.log(`Getting all files in directory ${target}...`);
-  const paths = walkSync(target);
-  console.log(`Found ${paths.length} files.`);
-  writeFileSync(output, paths.join("\n"));
-  console.log(`Wrote output to ${output}`);
+export const dir2csv = async (
+  target: string,
+  outputCSVPath: string,
+  includeStats: boolean
+) => {
+  if (!existsSync(target)) {
+    Logger.error(`Target directory does not exist: ${target}`);
+  } else {
+    Logger.log(`Getting all files in directory ${target}`);
+    let headers = [];
+    let rows = [];
+    if (includeStats) {
+      const paths = walkSync.entries(target);
+      headers = [
+        "relativePath",
+        "basePath",
+        "mode",
+        "size",
+        "mtime",
+        "isDirectory",
+      ];
+      rows = paths.map((p) => {
+        return {
+          relativePath: p.relativePath,
+          basePath: p.basePath,
+          mode: p.mode,
+          size: p.size,
+          mtime: p.mtime,
+          isDirectory: p.isDirectory().toString(),
+        };
+      });
+    } else {
+      headers = ["path"];
+      rows = walkSync(target).map((p) => {
+        return { path: p };
+      });
+    }
+
+    Logger.log(`Found ${rows.length} files.`);
+
+    await writeCSVWithRetryPrompt(outputCSVPath, headers, rows);
+  }
 };

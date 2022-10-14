1b7d440431a8ab68f95f7553457931ec67c38276
Erik Hauck <erikmhauck@gmail.com>
2022-10-13 21:48:44 -0500

    better dirdiff

--- src/cli/dirdiff.ts
+++ src/cli/dirdiff.ts
@@ -5,22 +5,22 @@ import { writeCSVWithRetryPrompt } from "../api/csvUtils.js";
 import { walk } from "../api/walk.js";
 
 const printPathsInXNotInY = (
-  paths: string[],
+  paths: IPathComparison[],
   inTargetName: string,
   notInTargetName: string
 ) => {
+  const inTargetNameBold = chalk.bold(inTargetName);
+  const notInTargetNameBold = chalk.bold(notInTargetName);
   if (paths.length > 0) {
     Logger.error(
-      `${paths.length} files in ${chalk.bold(
-        inTargetName
-      )} that are not present in ${chalk.bold(notInTargetName)}`
+      `${paths.length} files in ${inTargetNameBold} that are not present in ${notInTargetNameBold}`
     );
     paths.forEach((d) => {
-      Logger.log(`-- ${path.join(inTargetName, d)}`);
+      Logger.log(`-- ${d.fullPath}`);
     });
   } else {
     Logger.success(
-      `No files in ${inTargetName} that are absent from ${notInTargetName}`
+      `No files in ${inTargetNameBold} that are absent from ${notInTargetNameBold}`
     );
   }
 };
@@ -30,16 +30,37 @@ const getFilePathWithoutExtension = (filePath) => {
   return path.join(p.dir, p.name);
 };
 
-const pathInArray = (
-  fileName: string,
-  fileArrayToCheck: string[],
+interface IPathComparison {
+  fullPath: string;
+  relativePath: string;
+  comparisonPath: string;
+  targetName: string;
+  hasMatch: boolean;
+}
+
+const getComparisonPathsFromTarget = async (
+  targetName: string,
   ignoreExtensions: boolean
-) => {
-  if (ignoreExtensions) {
-    return fileArrayToCheck.includes(getFilePathWithoutExtension(fileName));
-  } else {
-    return fileArrayToCheck.includes(fileName);
-  }
+): Promise<IPathComparison[]> => {
+  const paths = (await walk(targetName)).map((p) => p.path);
+  return paths
+    .filter((filePath) => filePath !== targetName)
+    .map((filePath) => {
+      const relativePath = filePath.replace(targetName, "");
+      let comparisonPath = relativePath;
+      if (comparisonPath) {
+        if (ignoreExtensions) {
+          comparisonPath = getFilePathWithoutExtension(comparisonPath);
+        }
+        return {
+          fullPath: filePath,
+          comparisonPath,
+          targetName,
+          relativePath,
+          hasMatch: false,
+        };
+      }
+    });
 };
 
 export const dirdiff = async (
@@ -48,38 +69,37 @@ export const dirdiff = async (
   ignoreExtensions: boolean,
   outputCSVPath: string
 ) => {
-  const paths1 = (await walk(target1)).map((p) => p.path);
-  const paths2 = (await walk(target2)).map((p) => p.path);
-  let paths1ToCompare = paths1;
-  let paths2ToCompare = paths2;
-  if (ignoreExtensions) {
-    paths1ToCompare = paths1.map((p) => getFilePathWithoutExtension(p));
-    paths2ToCompare = paths2.map((p) => getFilePathWithoutExtension(p));
-  }
+  const paths1 = await getComparisonPathsFromTarget(target1, ignoreExtensions);
+  const paths2 = await getComparisonPathsFromTarget(target2, ignoreExtensions);
 
-  const inTarget1Only = paths1.filter(
-    (filePath) => !pathInArray(filePath, paths2ToCompare, ignoreExtensions)
-  );
-  const inTarget2Only = paths2.filter(
-    (filePath) => !pathInArray(filePath, paths1ToCompare, ignoreExtensions)
-  );
+  const inTarget1Only = [];
+  const inTarget2Only = [];
+
+  paths1.forEach((p1) => {
+    if (paths2.some((p2) => p1.comparisonPath === p2.comparisonPath)) {
+      p1.hasMatch = true;
+    } else {
+      inTarget1Only.push(p1);
+    }
+  });
+
+  paths2.forEach((p2) => {
+    if (paths1.some((p1) => p2.comparisonPath === p1.comparisonPath)) {
+      p2.hasMatch = true;
+    } else {
+      inTarget2Only.push(p2);
+    }
+  });
 
   printPathsInXNotInY(inTarget1Only, target1, target2);
   printPathsInXNotInY(inTarget2Only, target2, target1);
 
   if (outputCSVPath) {
-    const headers = ["targetName", "filePath"];
-    const rows1 = paths1.map((p) => {
-      return { filePath: p, targetName: target1 };
-    });
-    const rows2 = paths2.map((p) => {
-      return { filePath: p, targetName: target2 };
-    });
-    const rows = rows1.concat(rows2);
+    const headers = ["targetName", "relativePath", "hasMatch"];
+    const rows = paths1.concat(paths2);
     rows.sort((a, b) => {
-      return a.filePath.localeCompare(b.filePath);
+      return a.relativePath.localeCompare(b.relativePath);
     });
-
     await writeCSVWithRetryPrompt(outputCSVPath, headers, rows);
   }
 };

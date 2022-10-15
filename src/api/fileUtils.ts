2866c1f60923483602bcc7bf6ab0d0f20cffdc01
Erik Hauck <erikmhauck@gmail.com>
2022-10-15 13:50:59 -0500

    add option to skip to fileUtils

--- src/api/fileUtils.ts
+++ src/api/fileUtils.ts
@@ -3,29 +3,61 @@ import fs from "fs";
 import Logger from "./logger.js";
 import inquirer from "inquirer";
 
-const shouldTryAgain = async () => {
-  const { tryAgain } = await inquirer.prompt([
+const shouldTryAgain = async (
+  oldPath: string,
+  newPath: string,
+  action: string,
+  dontTryAgainChoiceText
+) => {
+  const oldPathFormatted = Logger.getFormattedPath(oldPath);
+  const newPathFormatted = Logger.getFormattedPath(newPath);
+  const answers = await inquirer.prompt([
     {
       name: "tryAgain",
-      message: `Try again?`,
-      type: "confirm",
+      message: " ",
+      prefix: "",
+      type: "list",
+      choices: [
+        {
+          name: `Try to ${action} ${oldPathFormatted} to ${newPathFormatted} again`,
+          value: true,
+        },
+        { name: dontTryAgainChoiceText, value: false },
+      ],
     },
   ]);
-  return tryAgain;
+  return answers.tryAgain;
 };
 
-export const moveFileWithRetryPrompt = (oldPath: string, newPath: string) => {
-  Logger.log(`Moving ${oldPath} to ${newPath}`);
-  executeWithPromptForRetry(
+export const moveFileWithRetryPrompt = async (
+  oldPath: string,
+  newPath: string,
+  dontTryAgainChoiceText = "Skip"
+) => {
+  const action = "move";
+  const success = await executeWithPromptForRetry(
     () => fs.renameSync(oldPath, newPath),
-    shouldTryAgain
+    async () =>
+      await shouldTryAgain(oldPath, newPath, action, dontTryAgainChoiceText),
+    `Failed to ${action} ${oldPath} to ${newPath}`
   );
+  if (!success) {
+    Logger.log(`Skipping ${oldPath}`);
+  }
+  return success;
 };
 
-export const copyFileWithRetryPrompt = (oldPath: string, newPath: string) => {
-  Logger.log(`Copying ${oldPath} to ${newPath}`);
-  executeWithPromptForRetry(
+export const copyFileWithRetryPrompt = async (
+  oldPath: string,
+  newPath: string,
+  dontTryAgainChoiceText = "Skip"
+) => {
+  const action = "copy";
+  const success = await executeWithPromptForRetry(
     () => fs.copyFileSync(oldPath, newPath),
-    shouldTryAgain
+    async () =>
+      await shouldTryAgain(oldPath, newPath, "copy", dontTryAgainChoiceText),
+    `Failed to ${action} ${oldPath} to ${newPath}`
   );
+  return success;
 };
